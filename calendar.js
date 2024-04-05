513203a197dcc160eeaf158d3fb815114c2d48b7
Nicolas <nicolas.tonassi@alunoescolassociais.org.br>
2024-04-05 09:03:32 -0300

    Alteração no Javscript

--- calendar.js
+++ calendar.js
@@ -1,9 +1,13 @@
-function colorirDia(){
-    let days        = document.getElementById("day").value;
-    let color       = document.getElementById('color').value;
-    let calendar    = document.getElementById('calendar');
-    let td          = calendar.getElementsByTagName('td')[(parseInt(days)-1)];
+function colorirDia() {
+    let days = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendar');
+    let td = calendar.getElementsByTagName('td')[(parseInt(days) - 1)];
+ 
+    if (days < 1 || days > 30) {
+        alert("Por favor, digite um número de dia válido entre 01 e 30.");
+        return;
+    }
  
     td.style.backgroundColor = color;
- 
 }
